144536a1659b866359e75bd856594df044c80ddf
the-dagger <harshithdwivedi@gmail.com>
2018-10-07 21:05:47 +0530

    add notification support
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

--- QtNotifier/android/src/ru/notifications/javalib/QtAndroidNotifications.java
+++ QtNotifier/android/src/ru/notifications/javalib/QtAndroidNotifications.java
@@ -1,9 +1,11 @@
 package ru.notifications.javalib;
 
 // Qt
+
 import org.qtproject.qt5.android.QtNative;
 
 // android
+import android.app.AlarmManager;
 import android.content.Intent;
 import android.content.Context;
 import android.app.PendingIntent;
@@ -15,6 +17,21 @@ import java.lang.String;
 
 class QtAndroidNotifications {
 
+
+    //TODO : Send the EPOCH time in milliseconds to this method at which the alarm is to be scheduled
+    public static void scheduleNotification(long time) {
+        Context context = QtNative.activity();
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+        Intent intent = new Intent(context, NotificationReceiver.class);
+
+        PendingIntent pi = PendingIntent.getBroadcast(context, 12345, intent, PendingIntent
+                .FLAG_UPDATE_CURRENT);
+
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, time, pi);
+
+    }
+
     public static void show(String title, String caption, int id) {
         System.out.println("show");
 
@@ -23,21 +40,20 @@ class QtAndroidNotifications {
         NotificationManager notificationManager = getManager();
         Notification.Builder builder =
                 new Notification.Builder(context)
-                .setSmallIcon(android.R.drawable.ic_delete)
-                .setContentTitle(title)
-                .setContentText(caption)
-                .setAutoCancel(true)
-                ;
+                        .setSmallIcon(android.R.drawable.ic_delete)
+                        .setContentTitle(title)
+                        .setContentText(caption)
+                        .setAutoCancel(true);
 
         String packageName = context.getApplicationContext().getPackageName();
         Intent resultIntent = context.getPackageManager().getLaunchIntentForPackage(packageName);
         resultIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
 
         PendingIntent resultPendingIntent =
-            PendingIntent.getActivity(
-            context, 0,
-            resultIntent, PendingIntent.FLAG_UPDATE_CURRENT
-        );
+                PendingIntent.getActivity(
+                        context, 0,
+                        resultIntent, PendingIntent.FLAG_UPDATE_CURRENT
+                );
 
         builder.setContentIntent(resultPendingIntent);
         notificationManager.notify(id, builder.build());
